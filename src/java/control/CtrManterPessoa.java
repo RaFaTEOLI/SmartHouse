6a2d2b213212461f72e0d6cf8b34b09f2b96e86e
Rafael Tessarolo <40833512+RaFaTEOLI@users.noreply.github.com>
2019-11-05 20:38:04 -0300

    Update CtrManterPessoa.java

--- src/java/control/CtrManterPessoa.java
+++ src/java/control/CtrManterPessoa.java
@@ -51,23 +51,11 @@ public class CtrManterPessoa {
     }
     
     public String alterar() {
-        Long temUsuario = daoPessoa.validarUsuario(pessoa.getUsuario(), pessoa.getSenha());
-        System.out.println("LOG STATUS | temUsuario: " + temUsuario);
-        if (temUsuario != 0) {
-            System.out.println("LOG STATUS | Usuário Duplicado");
-            pessoa = new Pessoa();
-            FacesContext.getCurrentInstance().addMessage(
-                    null,
-                    new FacesMessage(FacesMessage.SEVERITY_ERROR, "Usuário já existe!",
-                    "Erro na inserção!"));
-            return "usuarioDuplicado";
-        } else {
-            try {
-                daoPessoa.alterar(pessoa);
-                return "alt";
-            } catch (HibernateException e) {
-                return "falha";
-            }
+        try {
+            daoPessoa.alterar(pessoa);
+            return "alt";
+        } catch (HibernateException e) {
+            return "falha";
         }
     }
     
