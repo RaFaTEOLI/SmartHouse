3d9f3bcf7a328d9afa10b91283efe75b55c84952
Rafael Tessarolo <rafinha.tessarolo@hotmail.com>
2019-11-08 18:00:30 -0300

    Version 1.0

--- src/java/dao/DaoMorador.java
+++ src/java/dao/DaoMorador.java
@@ -12,7 +12,7 @@ public class DaoMorador extends DaoGenerico {
         Session session = hibernateConfiguracao.openSession();
         Transaction transaction = session.beginTransaction();
         
-        org.hibernate.Query query = session.createQuery("SELECT COUNT(*) FROM Morador WHERE moradorId = '" + moradorId + "' AND casaId = '" + casaId + "'");
+        org.hibernate.Query query = session.createQuery("SELECT COUNT(*) FROM Morador WHERE pessoaId = '" + moradorId + "' AND casaId = '" + casaId + "'");
         System.out.println("LOG STATUS | Selecionando se a Pessoa: " + moradorId + " já é morador da casa: " + casaId + "... ");
         System.out.println("LOG QUERY | " + query);
         System.out.println("LOG RETURN | " + (Long) query.uniqueResult());
